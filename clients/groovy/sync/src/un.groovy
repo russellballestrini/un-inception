6d15dbdb168c6c5b47e35c554489eef2e5ca4e39
russell@unturf.com <russell@unturf.com>
2026-01-29 18:40:49 -0500

    chore: bump version to 4.2.50

--- clients/groovy/sync/src/un.groovy
+++ clients/groovy/sync/src/un.groovy
@@ -73,7 +73,7 @@
  * </ol>
  *
  * @author Permacomputer Project
- * @version 4.2.49
+ * @version 4.2.50
  */
 
 import javax.crypto.Mac
--- clients/python/sync/src/__init__.py
+++ clients/python/sync/src/__init__.py
@@ -27,7 +27,7 @@ from .un import (
     CredentialsError,
 )
 
-__version__ = "4.2.49"
+__version__ = "4.2.50"
 __all__ = [
     "execute_code",
     "execute_async",
